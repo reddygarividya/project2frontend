576ee12f80ed1776531f45ac072e4785675dcfae
Test <Test@DESKTOP-J1B7EKC>
2018-02-26 00:10:30 -0700

    images

--- WebContent/controllers/usercontroller.js
+++ WebContent/controllers/usercontroller.js
@@ -12,6 +12,7 @@ app.controller('UserController',function($scope,$rootScope,$location,UserService
 			$scope.error=response.data
 		})
 		
+		
 	}
 	$scope.login=function(user){
 		console.log('UserController -> login')
